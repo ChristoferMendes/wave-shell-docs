777c7588239f41ce48325e2f4f4c50b975bc0346
Christofer <christoferluizdsm@gmail.com>
2024-02-10 03:16:34 -0300

    feat: add advanced usage with surfstar and new apis doc

--- components/surfstar-usage-file-tree.tsx
+++ components/surfstar-usage-file-tree.tsx
@@ -1,19 +1,22 @@
 import { FileTree } from "nextra/components";
 
-export function Tree() {
+export function SurfstarUsageFileTree() {
   return (
     <FileTree>
       <FileTree.Folder name="bin">
-        <FileTree.File name="wave.ts" />
+        <FileTree.File name="wave" />
       </FileTree.Folder>
       <FileTree.Folder name="src" defaultOpen>
-        <FileTree.Folder name="components">
-          <FileTree.File name="hello.ts" />
+        <FileTree.Folder name="commands" defaultOpen>
+          <FileTree.File name="send-email.ts" />
+        </FileTree.Folder>
+        <FileTree.Folder name="templates" defaultOpen>
+          <FileTree.File name="course.surf" />
         </FileTree.Folder>
         <FileTree.File name="package.json" />
         <FileTree.File name="bun.lockb" />
         <FileTree.File name="README.md" />
       </FileTree.Folder>
     </FileTree>
-  );
+  )
 }
